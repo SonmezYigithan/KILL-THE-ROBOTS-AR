4aae2d8ea04c117d79fdb90a14ae711376de6f45
emreozturk017 <emreozturk0017@gmail.com>
2020-12-27 16:45:49 +0300

    0.03
    Enemy Healt Bar and Shooting Script

---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject smoke; 
+
+    public void Shoot()
+    {
+        RaycastHit hit; 
+
+        if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            if(hit.transform.name == "Enemy")
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
