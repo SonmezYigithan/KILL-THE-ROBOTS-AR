ac722f459f2acb7d4906bcf569de848b9d4726b7
SonmezYigithan <sonmez.yigithan@gmail.com>
2021-01-05 20:34:17 +0300

    Revert "pistol"
    This reverts commit 59247c1566405907e9dc3ddbb6fe954827458a26.

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -9,23 +9,10 @@ public class ShootingScript : MonoBehaviour
 
     public static int EnemiesKilled = 0;
 
-<<<<<<< Updated upstream
-=======
-    public GameObject Pistol;
-    private Animator animator;
-
-    void Start()
-    {
-        spawnScriptObj = GameObject.FindGameObjectWithTag("SpawnEdge");
-        SpawnScript = spawnScriptObj.GetComponent<SpawnTheRobots>();
-        animator = Pistol.GetComponent<Animator>();
-    }
-
->>>>>>> Stashed changes
     public void Shoot()
     {
         RaycastHit hit;
-        animator.SetBool("isShooting", true);
+
         if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
         {
             if (hit.transform.tag == "Enemy")
@@ -42,20 +29,7 @@ public class ShootingScript : MonoBehaviour
             else if(hit.transform.tag == "Health Potion")
             {
                 Destroy(hit.transform.gameObject);
-<<<<<<< Updated upstream
-=======
-                GameControllerScript.HitHealthPotion();
-            }
-            else if(hit.transform.tag == "Boss1")
-            {
-                Boss1Script.Boss1_HP -= 200;
->>>>>>> Stashed changes
             }
         }
-    }
-
-    public void ShootanimStop()
-    {
-        animator.SetBool("isShooting", false);
     }
 }
