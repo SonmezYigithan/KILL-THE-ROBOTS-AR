f365c96fe8d4cc16494a3c23b57ae2ed3ccd0137
SonmezYigithan <sonmez.yigithan@gmail.com>
2021-01-10 23:35:24 +0300

    Son Rütuşlar

--- Assets/Scripts/Boss Scripts/Boss1/Boss1Script.cs
+++ Assets/Scripts/Boss Scripts/Boss1/Boss1Script.cs
@@ -10,8 +10,8 @@ public class Boss1Script : MonoBehaviour
     // belli süre aralıklarıyla ataş edecek 
     // random animasyonlar olacak
 
-    public static float Boss1_HP_First = 100;
-    public static float Boss1_HP = 100;
+    public static float Boss1_HP_First = 500;
+    public static float Boss1_HP = 500;
 
     //HP BAR
     private float maxHP;
